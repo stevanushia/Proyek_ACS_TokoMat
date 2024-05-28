b7066428b2224f0ea589b99c06d71665f8a9adab
Stevanus Hia <stevanushiaa@gmail.com>
2024-05-28 19:34:38 +0700

    remake dataset, fixing db [barang], added logic and view on Master_Barang

--- Proyek_ACS_TokoMat/Admin/Master Barang.cs
+++ Proyek_ACS_TokoMat/Admin/Master Barang.cs
@@ -16,5 +16,68 @@ namespace Proyek_ACS_TokoMat.Admin
         {
             InitializeComponent();
         }
+
+        private void Master_Barang_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        void reset()
+        {
+            DataTable detail = DB.get($"SELECT * FROM BARANG WHERE NAMA LIKE '%{searchBar.Text}%' ORDER BY ID DESC;");
+            dgvBarang.DataSource = null;
+            dgvBarang.DataSource = detail;
+
+            foreach (DataGridViewRow r in dgvBarang.Rows)
+            {
+                if ((int)r.Cells["QTY"].Value <= 0)
+                {
+                    r.DefaultCellStyle.ForeColor = Color.Red;
+                    string id = r.Cells["QTY"].Value.ToString();
+                }
+            }
+
+            btnUpdate.Enabled = false;
+            btnClear.Enabled = true;
+            btnInput.Enabled = true;
+
+            numQty.Value = 0;
+            numHarga.Value = 0;
+            numBeli.Value = 0;
+
+            textBoxNama.Text = "";
+
+            cbStatus.SelectedIndex = 0;
+
+            textBoxId.Text = DB.generateId("BARANG");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            string id = textBoxId.Text;
+            string nama = textBoxNama.Text;
+            int harga = (int)numHarga.Value;
+            int hbeli = (int)numBeli.Value;
+            int qty = (int)numQty.Value;
+            string status = cbStatus.Text;
+
+            if (nama != "" && harga > 0 && hbeli > 0 && cbStatus.SelectedIndex > -1)
+            {
+                if (DB.cekNamaKembar("BARANG", nama))
+                {
+                    MessageBox.Show($"Nama {nama} sudah dipakai");
+                    return;
+                }
+
+                DB.exec($"INSERT INTO BARANG VALUES('{id}','{nama}','{qty}','{harga}', '{hbeli}' ,'{status}')");
+                reset();
+            }
+            else MessageBox.Show("Ada field kosong");
+        }
     }
 }
